3244c6eb6241f7e2c0f3b94f69bdf6223f695611
zhaiyuting <3208253104@qq.com>
2018-08-14 09:22:56 +0800

    shiro_demo第二次提交

---
+++ src/main/java/com/gentlehu/shiro/Test2.java
@@ -0,0 +1,7 @@
+package com.gentlehu.shiro;
+
+/**
+ * Created by DELL on 2018/8/14.
+ */
+public class Test2 {
+}
--- src/main/java/com/gentlehu/shiro/TestAdd.java
+++ src/main/java/com/gentlehu/shiro/TestAdd.java
@@ -4,4 +4,7 @@ package com.gentlehu.shiro;
  * Created by DELL on 2018/8/14.
  */
 public class TestAdd {
+    public static void main(String[] args) {
+
+    }
 }
